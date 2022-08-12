72840a31b3c84b24a34635c87408ceaa4ee8d806
JordGreer <jordgreer@gmail.com>
2022-08-12 09:50:36 -0400

    Create mint button
    Copied the set purpose button to start a mint button.

--- packages/react-app/src/views/ExampleUI.jsx
+++ packages/react-app/src/views/ExampleUI.jsx
@@ -59,6 +59,32 @@ export default function ExampleUI({
           >
             Set Purpose!
           </Button>
+          <Button
+            style={{ marginTop: 8 }}
+            onClick={async () => {
+              /* look how you call setPurpose on your contract: */
+              /* notice how you pass a call back for tx updates too */
+              const result = tx(writeContracts.YourContract.setPurpose(newPurpose), update => {
+                console.log("📡 Transaction Update:", update);
+                if (update && (update.status === "confirmed" || update.status === 1)) {
+                  console.log(" 🍾 Transaction " + update.hash + " finished!");
+                  console.log(
+                    " ⛽️ " +
+                      update.gasUsed +
+                      "/" +
+                      (update.gasLimit || update.gas) +
+                      " @ " +
+                      parseFloat(update.gasPrice) / 1000000000 +
+                      " gwei",
+                  );
+                }
+              });
+              console.log("awaiting metamask/web3 confirm result...", result);
+              console.log(await result);
+            }}
+          >
+            Mint!
+          </Button>
         </div>
         <Divider />
         Your Address:
