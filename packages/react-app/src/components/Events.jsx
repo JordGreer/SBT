592421e1b766efa8c4bd61a2ea016386fc288f12
JordGreer <jordgreer@gmail.com>
2022-08-18 14:22:35 -0400

    Update events
    Changed the format of the header and removed "item.args.purpose" in the key for the list.  I believe this was used to prioritize the list items for the updating purpose example in scaffold eth.

--- packages/react-app/src/components/Events.jsx
+++ packages/react-app/src/components/Events.jsx
@@ -25,15 +25,14 @@ export default function Events({ contracts, contractName, eventName, localProvid
 
   return (
     <div style={{ width: 600, margin: "auto", marginTop: 32, paddingBottom: 32 }}>
-      <h2>Events:</h2>
+      <h2>{eventName}</h2>
       <List
         bordered
         dataSource={events}
         renderItem={item => {
           return (
-            <List.Item key={item.blockNumber + "_" + item.args.sender + "_" + item.args.purpose}>
+            <List.Item key={item.blockNumber + "_" + item.args.sender + "_"}>
               <Address address={item.args[0]} ensProvider={mainnetProvider} fontSize={16} />
-              {eventName}
             </List.Item>
           );
         }}
