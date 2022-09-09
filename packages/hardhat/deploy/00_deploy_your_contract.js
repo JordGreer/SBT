e88610c53f72067301176e1bab73fa68dafd6df2
JordGreer <jordgreer@gmail.com>
2022-09-09 16:08:14 -0400

    Working on a Factory
    I am attempting to create a factory contract that keeps track of individual SBTs.  Not sure if I need to create a second contract to achieve this.  Also having difficulties deploying to hardhat.  I think this is due to now having two contracts.  Created second branch to start this idea.  I may abandon this idea if I can keep everything on one contract.

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -20,7 +20,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   await deploy("YourContract", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    args: ["BuidlSoul", "BS"],
+    args: [],
     log: true,
     waitConfirmations: 5,
   });
